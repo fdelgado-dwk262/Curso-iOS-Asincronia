54f2bf281c51244852b149f72b8ee7df9dc890e3
Fernando <dwk262@gmail.com>
2026-02-10 09:42:26 +0100

    Initial Commit

---
+++ Curso-iOS-Asincronia/Curso_iOS_AsincroniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_AsincroniaApp.swift
+//  Curso-iOS-Asincronia
+//
+//  Created by Equipo 9 on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_AsincroniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
